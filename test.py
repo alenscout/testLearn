8af7e8bc0228e10cf81082a0e25c86968d1f3f70
alenscout <a13nscout@gmail.com>
2024-02-23 16:53:41 +0600

    func(rebz)

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ print(request1.status_code)
 print("hiiii")
 albania = [1,2,3,4,5]
 greece = "1,2,3,4,5"
+
+def func(rebz):
+    if rebz != 0:
+        return rebz
+    else:
+        return "error"
+print(func(0))
